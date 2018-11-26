a0f4773d55adfbfd378693a918e83bcf3c99c9d5
samomar <8659480+samomar@users.noreply.github.com>
2018-11-26 16:16:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-import random
 import operator
 import time
 
@@ -21,9 +20,34 @@ seasonPopup = driver.find_element_by_id('seasonPopup')
 products_dict = {}
 upgrades_dict = {}
 
+list_of_zero_nums = {
+    'million': 6,
+    'billion': 9,
+    'trillion': 12,
+    'quadrillion': 15,
+    'quintillion': 18,
+    'sextillion': 21,
+    'septillion': 24,
+    'octillion': 27,
+    'nonillion': 30,
+    'decillion': 33,
+}
+
+
+def number_cleaner(number):
+    for key, value in list_of_zero_nums.items():
+        if key in number:
+            grab = list_of_zero_nums.get(key, 1)
+            zeros = ''
+            for i in range(grab):
+                zeros += '0'
+            result = int(number.replace(key, zeros).replace(' ', '').replace('.', '').replace(',', ''))
+            return result
+    return number
+
 
 def current_cookies():
-    cookies = driver.find_element_by_id('cookies').text.split(' ')[0].replace(',', '')
+    cookies = number_cleaner(driver.find_element_by_id('cookies').text.split('cookies')[0])
     return int(cookies)
 
 
@@ -46,9 +70,10 @@ except Exception as e:
 
 def price_checker(entity):
     ActionChains(driver).move_to_element(entity).perform()
-    product_price = driver.find_element_by_id('tooltip').find_element_by_class_name("price").text.replace(',', '')
+    price = driver.find_element_by_id('tooltip').find_element_by_class_name("price").text.replace(',', '')
+    product_price = number_cleaner(price)
     product_name = driver.find_element_by_id('tooltip').find_element_by_class_name("name").text
-    return int(product_price), product_name
+    return product_price, product_name
 
 
 def product_checker():
@@ -57,11 +82,13 @@ def product_checker():
         products_enabled = driver.find_elements_by_xpath('//*[@class="product unlocked enabled"]')
         if products_enabled:
             for product in reversed(products_enabled):
-                price, name = price_checker(product)
-                if name:
-                    products_dict[name] = price
+                pass
+                product_data = price_checker(product)
+                price, name = product_data
+                if name and price:
+                    products_dict[name] = int(price)
                     if is_product_worthy(products_dict):
-                        product.click()  # fix click
+                        product.click()
 
 
 def upgrades_checker():
@@ -75,13 +102,13 @@ def upgrades_checker():
                 if price and name:
                     upgrades_dict[name] = price
 
-                    if max_product_value >= (upgrades_dict[name] * 3):
+                    if int(max_product_value) >= int(upgrades_dict[name] * 3):
                         upgrade.click()
 
 
 def is_product_worthy(entity_dict):
     max_entity_value = max(entity_dict.items(), key=operator.itemgetter(1))[1]
-    cookies = current_cookies()
+    cookies = int(current_cookies())
     if (cookies / 100 * 50) < max_entity_value:
         return False
     else:

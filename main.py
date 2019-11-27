e08030d178fd9c8971851c36bd60f4aadfb3e4c5
sam <ieslam@live.com>
2019-11-26 22:46:34 -0500

    adding changes from months ago

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from selenium.webdriver.common.action_chains import ActionChains
+import random
 import operator
 import time
 
-chrome_path = "your_chrome_path_here"
+# predict the time it'll take to unlock the next cookie generator object
+# if time < desired time, wait else click in (time / 2) or (time / 4)
+
+# looks like the more entities you have, the more upgrades you're able to unlock
+# try to prioritize cursors, clicking upgrades
+# todo: use async
+
+
+chrome_path = r"C:\Users\i\Desktop\chromedriver.exe"
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("user-data-dir=cookie-clicker")
@@ -13,14 +23,22 @@ driver = webdriver.Chrome(chrome_path, options=chrome_options)
 
 driver.get('http://orteil.dashnet.org/cookieclicker/')
 
+# make a static container for all elements that you only need to grab once
 big_cookie = driver.find_element_by_id('bigCookie')
 golden_cookie = driver.find_element_by_id('goldenCookie')
-seasonPopup = driver.find_element_by_id('seasonPopup')
+season_popup = driver.find_element_by_id('seasonPopup')
+
+# logic - if product costs 20% or less of total cookies, don't buy
+
+# static elements / don't always check for upgrade / product prices/data  this only updates rarely
+
+# calculate cookies per second rate - make a function that predicts time till you're able to purchase max value product
+# if time to purchase max product is less than 5 minutes, return false (don't purchase)
 
 products_dict = {}
 upgrades_dict = {}
 
-list_of_zero_nums = {
+list_of_zero_nums = {  # todo: this is useless, just make it click things visually or use OCR
     'million': 6,
     'billion': 9,
     'trillion': 12,
@@ -43,11 +61,13 @@ def number_cleaner(number):
                 zeros += '0'
             result = int(number.replace(key, zeros).replace(' ', '').replace('.', '').replace(',', ''))
             return result
-    return number
+    return number.replace(' ', '').replace('.', '').replace(',', '')
 
 
+# dynamic elements / this is constantly changing and always being scanned for
 def current_cookies():
     cookies = number_cleaner(driver.find_element_by_id('cookies').text.split('cookies')[0])
+    # cookies = driver.find_element_by_id('cookies').text.split(' ')[0].replace(',', '')
     return int(cookies)
 
 
@@ -58,13 +78,11 @@ def accept_cookie():
 
 try:
     accept_cookie()
-except Exception as e:
-    print(e)
+except NoSuchElementException:
+    time.sleep(2)
     try:
-        time.sleep(2)
         accept_cookie()
-    except Exception as e:
-        print(e)
+    except NoSuchElementException:
         pass
 
 
@@ -73,37 +91,32 @@ def price_checker(entity):
     price = driver.find_element_by_id('tooltip').find_element_by_class_name("price").text.replace(',', '')
     product_price = number_cleaner(price)
     product_name = driver.find_element_by_id('tooltip').find_element_by_class_name("name").text
-    return product_price, product_name
+    if product_price and product_name:
+        return product_price, product_name
 
 
 def product_checker():
     first_product = driver.find_element_by_css_selector('#product0').get_attribute('class')
     if first_product == 'product unlocked enabled':
         products_enabled = driver.find_elements_by_xpath('//*[@class="product unlocked enabled"]')
-        if products_enabled:
-            for product in reversed(products_enabled):
-                pass
-                product_data = price_checker(product)
-                price, name = product_data
-                if name and price:
-                    products_dict[name] = int(price)
-                    if is_product_worthy(products_dict):
-                        product.click()
+        for product in reversed(products_enabled):
+            price, name = price_checker(product)
+            products_dict[name] = int(price)
+            if is_product_worthy(products_dict):
+                product.click()
 
 
 def upgrades_checker():
-    max_product_value = max(products_dict.items(), key=operator.itemgetter(1))[1]
     first_upgrade = driver.find_element_by_css_selector('#upgrade0').get_attribute('class')
     if first_upgrade == 'crate upgrade enabled':
+        max_product_value = max(products_dict.items(), key=operator.itemgetter(1))[1]
         upgrades_enabled = driver.find_elements_by_xpath('//*[@class="crate upgrade enabled"]')
-        if upgrades_enabled:
-            for upgrade in upgrades_enabled:
-                price, name = price_checker(upgrade)
-                if price and name:
-                    upgrades_dict[name] = price
-
-                    if int(max_product_value) >= int(upgrades_dict[name] * 3):
-                        upgrade.click()
+        for upgrade in upgrades_enabled:
+            price, name = price_checker(upgrade)
+            if price and name:
+                upgrades_dict[name] = price
+            if int(max_product_value) > int(upgrades_dict[name]):
+                upgrade.click()
 
 
 def is_product_worthy(entity_dict):
@@ -135,16 +148,7 @@ cookie_clicker = """
 CookieClicker.start();
 """
 
-try:
-    driver.execute_script(cookie_clicker)
-except Exception as e:
-    print(e)
-    try:
-        time.sleep(2)
-        driver.execute_script(cookie_clicker)
-    except Exception as e:
-        print(e)
-        exit()
+driver.execute_script(cookie_clicker)
 
 while True:
     try:
@@ -152,5 +156,5 @@ while True:
         time.sleep(1)
         upgrades_checker()
         time.sleep(1)
-    except Exception as e:
-        print(e)
+    except (TypeError, ValueError, StaleElementReferenceException, NoSuchElementException):
+        pass
